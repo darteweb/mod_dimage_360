6ab887c9adcdf7cd622969bb099dcea213e55e40
D'arteweb <contact@darteweb.com>
2018-07-26 18:10:43 +0530

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,6 +8,7 @@
 defined('_JEXEC') or die('');
 
 $param = $params->get('logoFile');
+$position = $params->get('position');
 $doc = JFactory::getDocument();
 $doc->addScript( JURI::base().'modules/mod_dimage_360/tmpl/js/three.min.js' );
 $doc->addScript( JURI::base().'modules/mod_dimage_360/tmpl/js/photo-sphere-viewer.min.js' );
@@ -30,6 +31,14 @@ var PSV = new PhotoSphereViewer({
 		container: div,
 		time_anim: 3000,
 		navbar: true,
+		<?php
+		if(isset($position) && $position != '')
+		{
+			?>
+		default_position:{long: <?php echo $position ?>, lat: Math.PI/2},
+		<?php 
+		}
+		?>
 		navbar_style: {
 			backgroundColor: 'rgba(58, 67, 77, 0.7)'
 		},		
